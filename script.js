781aef475ce49a108d6a8507411559a3cc9df942
akashwagmode <akashwaghmode9080@gmail.com>
2024-12-19 14:33:24 +0530

    Js

---
+++ script.js
@@ -0,0 +1,16 @@
+let a=parseInt( prompt("enter a number"));
+let b=parseInt(prompt("enter a number"));
+var add=a+b;
+var sub=a-b;
+var mul=a*b;
+var div=a/b;
+var mod=a%b;
+
+alert("sum is add is "+add);
+alert("sum is sub is "+sub);
+alert("sum is mul is"+mul);
+alert("sum is div is"+div);
+alert("sum is mod is"+mod);
+
+ document.writeln(add);
+// console.log(sum);
